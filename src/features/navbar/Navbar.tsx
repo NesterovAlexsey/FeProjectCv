0353b6482b03c55c5f391e244da0c63bad3ded68
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-29 19:43:00 +0200

    added styles to the navbar element

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,13 +1,32 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { NavLink } from 'react-router-dom';
-// import styles from './Navbar.module.css';
+import styles from './Navbar.module.css';
 
 export default function Navbar(): JSX.Element {
+    const getStatus = (navData:{ isActive:boolean }):string =>
+    navData.isActive ? styles.active : styles.text;
+
 return (
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/info">Personal</NavLink>
-            <NavLink to="/repos">Repositorys</NavLink>
+        <nav className={styles.navBarTop}>
+            <ul className={styles.navBarList}>
+                <li className={styles.item}>
+                    <NavLink className={getStatus} to="/">Home</NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink className={getStatus} to="/info">About me</NavLink>
+                </li>
+                <li className={styles.item}>
+                    <NavLink className={getStatus} to="/repos">Repositories</NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
+
+// return (
+//     <nav className={styles.navBarList} id="headerActivePage">
+//         <NavLink className={styles.text} to="/">Home</NavLink>
+//         <NavLink className={styles.text} to="/info">About me</NavLink>
+//         <NavLink className={styles.text} to="/repos">Repositorys</NavLink>
+//     </nav>
+// );
