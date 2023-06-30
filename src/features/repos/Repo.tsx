a2cb34b7de41d7f82f521a3bd3da92af97d06de9
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-30 10:09:16 +0200

    added icons to the Repo.tsx

--- src/features/repos/Repo.tsx
+++ src/features/repos/Repo.tsx
@@ -7,38 +7,46 @@ import { loadRepos } from './repoSlice';
 import IRepo from './types/IRepo';
 
 export default function Repo(): JSX.Element {
-    const repos = useSelector(selectRepo);
-    const dispatch = useAppDispatch();
-    const reposWithFilter = repos.filter((element : IRepo) => element.description !== null);
+  const repos = useSelector(selectRepo);
+  const dispatch = useAppDispatch();
+  const reposWithFilter = repos.filter((element: IRepo) => element.description !== null);
 
-    useEffect(() => {
-        dispatch(loadRepos());
-      }, [dispatch]);
+  useEffect(() => {
+    dispatch(loadRepos());
+  }, [dispatch]);
 
-    // function getLogo(logo: string): string {
-    //   switch(logo) {
-    //     case "TypeScript":
-    //       return "TypeScript2";
-    //   }
-    // }
+  function getLogo(logo: string): string {
+    switch (logo) {
+      case 'TypeScript':
+        return './icons/Typescript_logo.png';
+      case 'JavaScript':
+        return './icons/Js_logo.png';
+      case 'Java':
+        return './icons/Java-Logo.png';
+      default:
+        return logo;
+    }
+  }
 
-    return (
-        <ul>
-        {reposWithFilter.map((el) => (
-          <li key={el.id}>
-            Project name: {el.name}
-            <br />
-            Project description: {el.description}
-            <br />
-            Code Language: {el.language}; {' '}
-            <Link to={el.html_url} target="_blank">
-            Check Code
-            </Link>
-            <br />
-            -------------------------------------
-          </li>
-        )
-        )}
-        </ul>
-    );
+  return (
+    <ul>
+      {reposWithFilter.map((el) => (
+        <li key={el.id}>
+          Project name: {el.name}
+          <br />
+          Project description: {el.description}
+          <br />
+          <div>
+            <img src={getLogo(el.language)} alt={el.language} />
+          </div>
+          <Link to={el.html_url} target="_blank">
+            Check Code!
+          </Link>
+          <br />
+          -------------------------------------
+        </li>
+      )
+      )}
+    </ul>
+  );
 }
